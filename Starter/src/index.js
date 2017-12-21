17d251bc1fdf836e8a13725081578126dd1853a2
Sameer Khan <sameer_khan@student.uml.edu>
2017-12-21 11:30:46 -0500

    Some updates

--- Starter/src/index.js
+++ Starter/src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAdXsATIgKvaYZrbgavdI8fbo6avdsq_Y0';
 
 // Create new component
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={ this.state.videos } />
+            </div>
+        );
+    }
 };
 
 // Inject this page and put it into DOM
