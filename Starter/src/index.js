c7e77971dc8ae525e887efd0e006e878bb9e8a85
Sameer Khan <sameer_khan@student.uml.edu>
2017-12-21 01:02:27 -0500

    Section 1 completed

--- Starter/src/index.js
+++ Starter/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAdXsATIgKvaYZrbgavdI8fbo6avdsq_Y0';
 
 // Create new component
 const App = () => {
-    return <div>Hi!</div>;
-}
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
 
 // Inject this page and put it into DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
