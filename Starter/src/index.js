b5f586fb7349ec0479d56800f466f1ccc5639d5b
Sameer Khan <sameer_khan@student.uml.edu>
2017-12-22 08:49:17 -0500

    Updated slightly

--- Starter/src/index.js
+++ Starter/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 
 const API_KEY = 'AIzaSyAdXsATIgKvaYZrbgavdI8fbo6avdsq_Y0';
 
@@ -14,7 +16,7 @@ class App extends Component {
         this.state = { videos: [] };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+            this.setState({videos});
         });
     }
 
@@ -22,7 +24,8 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={ this.state.videos } />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
