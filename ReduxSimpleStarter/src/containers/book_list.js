2183287b7ede1edffa04ed79d28ec591b4158989
Sameer Khan <sameer_khan@student.uml.edu>
2017-12-30 14:44:09 -0500

    Learnt about reducers and containers

--- ReduxSimpleStarter/src/containers/book_list.js
+++ ReduxSimpleStarter/src/containers/book_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect }from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
 
     renderList() {
         return this.props.books.map((book) => {
@@ -18,3 +19,12 @@ export default class BookList extends Component {
         );
     }
 }
+
+function mapStateToProps(state) {
+    // whatever is returned here will show up as props of BookList
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
