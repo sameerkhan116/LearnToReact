b2d5e6cbe3d2ca62a0f023cdad4273f8a17ec71d
Sameer Khan <sameer_khan@student.uml.edu>
2018-01-19 18:17:57 +0530

    Restarted redux

--- ReduxSimpleStarter/src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export function selectBook(book) {
-    // console.log('A book has been selected: ', book.title);
-    return {
-        type: 'BOOK_SELECTED',
-        payload: book
-    };
-}
--- ReduxSimpleStarter/src/components/app.js
+++ ReduxSimpleStarter/src/components/app.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 
-import BookList from '../containers/book_list';
-
 export default class App extends Component {
   render() {
     return (
-      <div>
-      <BookList />
-      </div>
+      <div>React simple starter</div>
     );
   }
 }
