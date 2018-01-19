4e9964344e42f0327bd1ba0a6edaafc8088da928
Sameer Khan <sameer_khan@student.uml.edu>
2018-01-19 19:09:32 +0530

    Basic reducer created

--- ReduxSimpleStarter/src/reducers/reducer_books.js
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -3,6 +3,6 @@ export default function() {
         {title: 'JavaScript: The Good Parts'},
         {title: 'Harry Potter'},
         {title: 'The Dark Tower'},
-        {title: 'Eloquent Ruby'},
-    ]
+        {title: 'Eloquent Ruby'}
+    ];
 }
