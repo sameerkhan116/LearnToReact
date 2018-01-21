2b6d3f04ddaea75617ee6fd0b4763fea0012e050
Sameer Khan <sameer_khan@student.uml.edu>
2018-01-21 13:28:32 +0530

    Weather app taking shape

--- ReduxSimple/src/reducers/index.js
+++ ReduxSimple/src/reducers/index.js
@@ -4,7 +4,7 @@ import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
   books: BooksReducer,
-  activeBook: ActiveBook
+  activeBook: ActiveBook 
 });
 
 export default rootReducer;
